912f0a7338428af974ba11db87e703efb1ee7173
Andraz Bajt <andraz@bajt.me>
2018-11-05 19:52:37 +0100

    Remove unneeded class bindings

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -10,13 +10,10 @@ import play.api.libs.Crypto
 class LoginController(ws: WSClient, fbConfig: LoginController.FacebookConfig, configSecret: String)
     extends Controller {
 
-  val appId = fbConfig.appId
-  val appSecret = fbConfig.appSecret
-
   val loginPage = Action { implicit request =>
     val redirectUri = routes.LoginController.loginCallback.absoluteURL()
     val fbUrl =
-      s"https://www.facebook.com/v2.8/dialog/oauth?client_id=$appId&redirect_uri=$redirectUri"
+      s"https://www.facebook.com/v2.8/dialog/oauth?client_id=${fbConfig.appId}&redirect_uri=$redirectUri"
     SeeOther(fbUrl)
   }
 
@@ -24,7 +21,7 @@ class LoginController(ws: WSClient, fbConfig: LoginController.FacebookConfig, co
     val redirectUri = routes.LoginController.loginCallback.absoluteURL()
     val code = request.queryString("code").head
     val tokenUrl =
-      s"https://graph.facebook.com/v2.8/oauth/access_token?client_id=$appId&redirect_uri=$redirectUri&client_secret=$appSecret&code=$code"
+      s"https://graph.facebook.com/v2.8/oauth/access_token?client_id=${fbConfig.appId}&redirect_uri=$redirectUri&client_secret=${fbConfig.appSecret}&code=$code"
     ws.url(tokenUrl).get().flatMap { tokenResponse =>
       val token = (tokenResponse.json \ "access_token").as[String]
       val meUrl = s"https://graph.facebook.com/v2.8/me?access_token=$token"
